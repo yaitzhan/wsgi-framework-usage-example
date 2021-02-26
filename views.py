7ab7d83212f441f9c364482366d33e5cae259e31
yaitzhan <y.aitzhanov@gmail.com>
2021-02-26 21:45:09 +0600

    modified views

--- views.py
+++ views.py
@@ -1,17 +1,30 @@
+import os
+
+from app_site import OnlineUniversitySite
+
 from wsgi_framework.views import BaseView
 from wsgi_framework.response import HTTPResponse
 from wsgi_framework.templating import render
+from wsgi_framework.logger import Logger
+
+
+app_site = OnlineUniversitySite()
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logger = Logger(name='app-logger', logs_dir='logs')
 
 
 class SimpleView(BaseView):
     def get(self, request):
-        content = render('index.html')
+        content = render('index.html', **request)
         return HTTPResponse(content)()
 
 
 class AboutView(BaseView):
     def get(self, request):
-        content = render('about.html')
+        content = render('about.html', **request)
         return HTTPResponse(content)()
 
 
@@ -25,16 +38,33 @@ class ContactsView(BaseView):
                                                                                params.get('message_title')))
         return HTTPResponse(content)()
 
+    def post(self, request):
+        body = request.get('body')
+        logger.info('Got POST data for ContactsView request: {}'.format(body))
+        return HTTPResponse()()
+
 
 class CreateCourseView(BaseView):
     def get(self, request):
-        content = render('create_course.html')
+        content = render('create_course.html', categories=app_site.categories)
+        return HTTPResponse(content)()
+
+    def post(self, request):
+        content = render('create_course.html', message='Новый курс добавлен!')
+        data = request.get('body')
+        logger.info('Got POST data for CreateCourseView request: {}'.format(data))
+        course_type = data.get('course_type')
+        course_name = data.get('course_name')
+        category_id = int(data.get('category_id'))
+        category_object = app_site.get_category_by_id(category_id)
+        app_site.create_course(course_name, category_object, course_type)
+        logger.info('Created new course')
         return HTTPResponse(content)()
 
 
 class CourseListView(BaseView):
     def get(self, request):
-        content = render('course_list.html')
+        content = render('course_list.html', objects_list=app_site.courses)
         return HTTPResponse(content)()
 
 
@@ -43,8 +73,17 @@ class CreateCategoryView(BaseView):
         content = render('create_category.html')
         return HTTPResponse(content)()
 
+    def post(self, request):
+        body = request.get('body')
+        logger.info('Got POST data for CreateCategoryView request: {}'.format(body))
+        name = body.get('name')
+        app_site.create_category(name)
+        content = render('create_category.html', message='Категория добавлена!')
+        logger.info('Created new category')
+        return HTTPResponse(content)()
+
 
 class CategoryListView(BaseView):
     def get(self, request):
-        content = render('category_list.html')
+        content = render('category_list.html', objects_list=app_site.categories)
         return HTTPResponse(content)()
